185ca823efa7d9bf97cc3890aa2e06ccc23a97ae
Oliver Zott <zott_oliver@web.de>
2020-12-10 20:14:15 +0100

    Adding example for combining pointcut-declarations

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -11,8 +11,16 @@ public class MyDemoLoggingAspect {
 
 	// Package name + class + method-wildcard ( 0 or more parmeters)
 	@Pointcut("execution(* com.luv2code.aopdemo.dao.MembershipDAO.*(..))")
-	private void forMembershipDAO() {
-	};
+	private void forMembershipDao() {
+	}
+
+	@Pointcut("execution(boolean com.luv2code.aopdemo.dao.MembershipDAO.*(..))")
+	private void noBoolean() {
+	}
+	
+	@Pointcut("forMembershipDao() && !noBoolean()")
+	private void forMembershipDaoNoBoolean() {
+	}
 
 	// "execution" is a point-cut!
 	@Before("execution(* add*(com.luv2code.aopdemo.Account, ..))")
@@ -25,12 +33,12 @@ public class MyDemoLoggingAspect {
 		System.out.println("\n=======>>>> Executon @Before advice on add*(), ONLY on return BOOLEAN");
 	}
 
-	@Before("forMembershipDAO()")
+	@Before("forMembershipDao()")
 	public void beforeAddMembershipAccount() {
 		System.out.println("\n=======>>>> Executon @Before advice on addAccount(), ONLY in MembershipDAO!");
 	}
 
-	@Before("forMembershipDAO()")
+	@Before("forMembershipDaoNoBoolean()")
 	public void performApiAnalytics() {
 		System.out.println("\n=======>>>> Executon @Before advice with POINTCUT DECLARATION");
 	}
