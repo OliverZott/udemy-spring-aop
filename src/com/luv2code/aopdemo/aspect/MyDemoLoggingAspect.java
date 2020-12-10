e76d9c0ea833486ce5179a2f83411cfa509298a3
Oliver Zott <zott_oliver@web.de>
2020-12-10 19:35:35 +0100

    Adding Pointcut Declaration

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,11 +2,17 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
+
+	// Package name + class + method-wildcard ( 0 or more parmeters)
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.MembershipDAO.*(..))")
+	private void forMembershipDAO() {
+	};
 
 	// "execution" is a point-cut!
 	@Before("execution(* add*(com.luv2code.aopdemo.Account, ..))")
@@ -14,16 +20,19 @@ public class MyDemoLoggingAspect {
 		System.out.println("\n=======>>>> Executon @Before advice on add*(), ONLY on correct PARAMETER + wildcard");
 	}
 
-	//
 	@Before("execution(boolean add*())")
 	public void beforeAddAccountAdviceBoolean() {
 		System.out.println("\n=======>>>> Executon @Before advice on add*(), ONLY on return BOOLEAN");
 	}
 
-	// Package name + class + method-wildcard ( 0 or more parmeters)
-	@Before("execution(* com.luv2code.aopdemo.dao.MembershipDAO.*(..))")
+	@Before("forMembershipDAO()")
 	public void beforeAddMembershipAccount() {
 		System.out.println("\n=======>>>> Executon @Before advice on addAccount(), ONLY in MembershipDAO!");
+	}
+
+	@Before("forMembershipDAO()")
+	public void performApiAnalytics() {
+		System.out.println("\n=======>>>> Executon @Before advice with POINTCUT DECLARATION");
 	}
 
 }
